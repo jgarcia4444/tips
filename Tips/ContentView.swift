315f94f0e615cb5f513546c00ab3957637bd0809
github username <github email address>
2020-07-28 17:11:02 -0700

    All form inputs added. amount per persoon next

--- Tips/ContentView.swift
+++ Tips/ContentView.swift
@@ -14,18 +14,39 @@ struct ContentView: View {
     @State var numberOfPeople = 2
     @State var tipPercentage = 2
     let tipPercentages = [0, 10, 15, 20, 25]
+    var amountPerPerson : Double {
+        return 0.0
+    }
     
     var body: some View {
-        NavigationView {
-            Form {
-                TextField("Check Amount", text: $checkAmount)
-                
+            NavigationView {
+                Form {
+                    Section {
+                        TextField("Check Amount", text: $checkAmount)
+                            .keyboardType(.decimalPad)
+                        Picker("Amount of People", selection: $numberOfPeople) {
+                            ForEach(2..<21) {
+                                Text("\($0) people")
+                            }
+                        }
+                    }
+                    
+                    Section(header: Text("What tip percentage do you want to apply?")) {
+                        Picker("Tip Percentage", selection: $tipPercentage) {
+                            ForEach(0..<tipPercentages.count) {
+                                Text("\(self.tipPercentages[$0])%")
+                            }
+                        }
+                    .pickerStyle(SegmentedPickerStyle())
+                    }
+                    
+                    Text("$\(amountPerPerson, specifier: "%.2f")" )
+                }
+                .navigationBarTitle("Tips")
             }
-            .navigationBarTitle("Tips")
+            .edgesIgnoringSafeArea(.all)
             
         }
-        .edgesIgnoringSafeArea(.all)
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
